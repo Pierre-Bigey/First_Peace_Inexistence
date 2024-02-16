e42ee424c5460d5f3f49d6b8ac3dbc03917b9303
Pierre Bigey <pierre.bigey1@gmail.com>
2024-02-16 20:54:42 +0100

    The weapon system works

---
+++ Assets/FirstPeace/Scripts/WeaponScript.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public float damage { get; private set; } = 10f;
+}
